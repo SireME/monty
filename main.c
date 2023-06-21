cbf39e285e4fe41c5c43e80b01a2f8f175e02e3c
Mbang Ernest N <mbangernest@yahoo.com>
2023-06-21 19:58:54 +0100

    implement push and pal functionality

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 int main(int argc, char **argv)
 {
 	stack_t *stack = NULL;
+	FILE *fl;
 
 	(void)argv;
 	if (argc != 2)
@@ -18,7 +19,14 @@ int main(int argc, char **argv)
 		write(2, "USAGE: monty file\n", 18);
 		exit(EXIT_FAILURE);
 	}
+	fl = open(argv[1], "r");
+	if (fl == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
+	fclose(fl);
 	push(&stack, 66);
 	pall(stack);
 
